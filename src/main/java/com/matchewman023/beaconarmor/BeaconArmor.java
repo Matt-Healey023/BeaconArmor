8777f63ed25fec036c7345e316296b8058a257f4
Matt-Healey023 <matthealey023@gmail.com>
2022-01-25 10:11:34 -0500

    Initial Commit

---
+++ src/main/java/com/matchewman023/beaconarmor/BeaconArmor.java
@@ -0,0 +1,8 @@
+package com.matchewman023.beaconarmor;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BeaconArmor implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
