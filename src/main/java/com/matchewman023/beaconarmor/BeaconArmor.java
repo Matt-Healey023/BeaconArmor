3006053d70f698ef1a0d49301449ee3ab235864e
Matt-Healey023 <matthealey023@gmail.com>
2022-01-25 19:42:06 -0500

    Created the new armor

--- src/main/java/com/matchewman023/beaconarmor/BeaconArmor.java
+++ src/main/java/com/matchewman023/beaconarmor/BeaconArmor.java
@@ -1,8 +1,11 @@
 package com.matchewman023.beaconarmor;
 
+import com.matchewman023.beaconarmor.registry.RegisterItems;
 import net.fabricmc.api.ModInitializer;
 
 public class BeaconArmor implements ModInitializer {
     @Override
-    public void onInitialize() {}
+    public void onInitialize() {
+        RegisterItems.register();
+    }
 }
